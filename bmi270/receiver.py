0a4cf4276b88bc3c0bb0bca8e9952a713beca860
ksmnt <b.contreras.2@ug.uchile.cl>
2024-05-03 18:41:03 -0400

    added python with some functions

--- bmi270/receiver.py
+++ bmi270/receiver.py
@@ -16,7 +16,7 @@ def send_message(message):
 def receive_response():
     """ Funcion para recibir un mensaje de la ESP32 """
     response = ser.readline()
-    return response
+    return response.decode()
 
 def receive_data():
     """ Funcion que recibe tres floats (fff) de la ESP32 
@@ -26,6 +26,17 @@ def receive_data():
     data = unpack("fff", data)
     print(f'Received: {data}')
     return data
+
+def change_mode(mode):
+    """ Funcion para cambiar el modo de operacion del sensor ESP32"""
+    if mode == 'low':
+        send_message('LOW')
+    elif mode == 'normal':
+        send_message('NORMAL')
+    elif mode == 'performance':
+        send_message('PERFORMANCE')
+    else:
+        print("Modo no reconocido")
 
 def send_end_message():
     """ Funcion para enviar un mensaje de finalizacion a la ESP32 """
