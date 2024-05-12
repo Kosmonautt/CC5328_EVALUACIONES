88f0a5f58e2a211f16da702145d49b5a94a67889
ksmnt <b.contreras.2@ug.uchile.cl>
2024-05-12 03:13:24 -0400

    the user can press enter in python to begin a new reading and added cache to git ignore

--- bmi270/receiver.py
+++ bmi270/receiver.py
@@ -1,4 +1,5 @@
 import serial
+from bmi_config import BMI_CONFIG
 from struct import pack, unpack
 
 # Se configura el puerto y el BAUD_Rate
@@ -13,10 +14,12 @@ def send_message(message):
     """ Funcion para enviar un mensaje a la ESP32 """
     ser.write(message)
 
-# def receive_response():
-#     """ Funcion para recibir un mensaje de la ESP32 """
-#     response = ser.readline()
-#     return response
+# Esta función también imprime en la consola de python para que el usuario siempre vea lo que se está recibiendo
+def receive_response():
+    """ Funcion para recibir un mensaje de la ESP32 """
+    response = ser.readline()
+    print(response)
+    return response
 
 # def receive_data():
 #     """ Funcion que recibe tres floats (fff) de la ESP32 
@@ -32,20 +35,37 @@ def send_message(message):
 #     end_message = pack('4s', 'END\0'.encode())
 #     ser.write(end_message)
 
-# Se envia el mensaje de inicio de comunicacion
-begin_message = pack('6s','BEGIN\0'.encode())
-send_message(begin_message)
+# Función que transforma un entero en un string de 3 caracteres
+def int_to_str(num):
+    return str(num).zfill(3)
 
-# # Se lee data por la conexion serial
-# counter = 0
-while True:
-    if ser.in_waiting > 0:
-        try:
-            response = ser.readline()
-            print(response)
-        except KeyboardInterrupt:
-            print('Finalizando comunicacion')
-            break
-        except:
-            print('Error en leer mensaje')
-            continue
+# # Se envia el mensaje de inicio de comunicacion
+# begin_message = pack('6s','BEGIN\0'.encode())
+# send_message(begin_message)
+
+# Función para el loop principal
+def loop():
+    # Se lee data por la conexion serial
+    while True:
+        if ser.in_waiting > 0:
+            try:
+                # se lee la data
+                response = receive_response()
+                # si el mensaje es b'Esperando inicio de lectura\r\n'
+                if response == b'Esperando inicio de lectura\r\n':
+                    # esperar a que el usuario presione enter
+                    input('Presiona enter para comenzar la lectura')
+                    # se envia el mensaje de inicio de lectura
+                    begin_message = pack('6s','BEGIN\0'.encode())
+                    send_message(begin_message)
+
+
+            except KeyboardInterrupt:
+                print('Finalizando comunicacion')
+                break
+            except:
+                print('Error en leer mensaje')
+                continue
+
+# Se ejecuta el loop principal
+loop()
