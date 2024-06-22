cfe1c14bc93bcaecdf470225dd91bb3e40754a92
ksmnt <b.contreras.2@ug.uchile.cl>
2024-06-21 20:06:26 -0400

    added new copy of GUI

---
+++ Tarea_2/GUI_NEW/main.py
@@ -0,0 +1,48 @@
+from embebidos import Ui_Dialog
+from PyQt5 import QtCore, QtGui, QtWidgets
+import serial_example as SE
+class Controller:
+
+    def __init__(self, parent):
+        self.ui = Ui_Dialog()
+        self.parent = parent
+
+    def setSignals(self):
+        self.ui.selec_12.currentIndexChanged.connect(self.leerModoOperacion)
+        self.ui.pushButton.clicked.connect(self.leerConfiguracion)
+        # self.ui.pushButton_2.clicked.connect(self.stop())
+
+    def leerConfiguracion(self):
+        conf = dict()
+        conf['AccSamp'] = self.ui.comboBox_acc_sampling.currentText()
+        conf['AccSen'] = self.ui.text_acc_sensibity.toPlainText()
+        print (conf)
+        return conf
+
+    def leerModoOperacion(self):
+        index = self.ui.selec_12.currentIndex()
+        texto = self.ui.selec_12.itemText(index)
+        print(texto)
+        return texto
+
+    def criticalError(self):
+        popup = QtWidgets.QMessageBox(parent= self.parent)
+        popup.setWindowTitle('Error Critico')
+        popup.setIcon(QtWidgets.QMessageBox.Icon.Critical)
+        popup.exec()
+        return
+
+    def stop(self):
+        print('Captando datos')
+        #SE.serial_cycle()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    cont = Controller(parent=Dialog)
+    ui = cont.ui
+    ui.setupUi(Dialog)
+    Dialog.show()
+    cont.setSignals()
+    sys.exit(app.exec_())
